f6070cf56bb9bfa23aca48f23f65b9819f90d276
Tony Onodi <tonyonodi@gmail.com>
2018-03-18 18:22:38 +0000

    Replaced css with styled components.

--- src/components/BookmarkletButton.js
+++ src/components/BookmarkletButton.js
@@ -1,7 +1,23 @@
 import React from "react";
+import styled from "styled-components"
 
-export default ({ code, text }) => (
-  <a href={code} className="bookmarklet">
-    {text}
-  </a>
+const BookmarkletView = styled.a`
+  color: #fff;
+  font-family: 'Inconsolata', monospace;  
+  text-decoration: none;
+  background: #c35050;
+  padding: 8px;
+  float: right;
+  margin: 4px 5px;
+  border-radius: 5px;
+`
+
+const EditIcon = styled.img`
+  display: inline;
+`
+
+export default ({ code, text, editable }) => (
+  <BookmarkletView href={code} className="bookmarklet">
+    <span>{text}</span> {editable ? <img src="/pen-square.svg" /> : null}
+  </BookmarkletView>
 );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,33 @@
 import React from "react";
+import styled from "styled-components";
 import BookmarkletButton from "./BookmarkletButton";
 import DevBookmarkletCode from "../lib/DevBookmarkletCode";
 
+const HeaderView = styled.div`
+  background-color: #1d1d1b;
+  height: 60px;
+  padding: 10px 20px;
+  color: white;
+`;
+
+const HeaderLogo = styled.h1`
+  display: inline-block;
+  margin: 0;
+  font-family: "Inconsolata", monospace;
+  -webkit-text-stroke-width: 1px;
+  -webkit-text-stroke-color: white;
+  font-size: 2rem;
+  background: white;
+  color: #1d1d1b;
+  padding: 0px 5px;
+  border-radius: 7px;
+  margin-top: 4px;
+`;
+
 export default ({ code, bookmarkletText }) => (
-  <div className="App-header">
-    <h1>BIDE</h1>
+  <HeaderView>
+    <HeaderLogo>BIDE</HeaderLogo>
     <BookmarkletButton code={DevBookmarkletCode} text="Dev Bookmarklet" />
-    <BookmarkletButton code={code} text={bookmarkletText} />
-  </div>
+    <BookmarkletButton code={code} text={bookmarkletText} editable />
+  </HeaderView>
 );
